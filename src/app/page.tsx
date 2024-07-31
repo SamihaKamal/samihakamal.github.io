d1b59fc00809fee591f95a61f0ef40b26c788ba5
SamihaKamal <samihakamal.7664@gmail.com>
2024-07-31 15:12:49 +0100

    added svg and did some animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
-
+import BubbleSVG from "./bubble";
 export default function Home() {
   return (
-    <h1>Hi</h1>
+    <main className="flex items-center justify-center min-h-screen">
+      <div style={{ width: '60vw', height: '45vw' }}>
+        <BubbleSVG className="w-full h-full" />
+      </div>
+    </main>
   );
 }
