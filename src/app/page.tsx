75e9363e6877cdad72df9a61d9f8438921d34416
SamihaKamal <samihakamal.7664@gmail.com>
2024-08-01 16:47:03 +0100

    Overhaul on how it looks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
-import BubbleSVG from "./bubble";
+import BubbleSVG from "./SVGComponents/bubble";
+import WaterSVG from "./SVGComponents/water";
+import WavesSVG from "./SVGComponents/waves";
+
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen">
-      <div style={{ width: '60vw', height: '45vw' }}>
-        <BubbleSVG className="w-full h-full" />
-      </div>
-    </main>
+    <main className=" bg-regal-blue relative min-h-screen min-w-screen overflow-hidden flex flex-col items-center justify-center">
+    <div className="absolute top-0 left-0" style={{ transform: "translateY(-75%) translateX(-30%)", width: "200vw" }}>
+      <WavesSVG />
+    </div>
+    <text className="text-9xl font-bold text-slate-300 " style={{fontFamily: "Poppins"}}>My portfolio</text>
+    <text className="absolute bottom-4 text-center text-6xl hover:text-8xl hover:cursor-pointer font-thin text-slate-300">v</text>
+  </main>
   );
 }
