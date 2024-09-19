8c850350fb85b42c8dff8d55543c761665c416fa
SamihaKamal <samihakamal.7664@gmail.com>
2024-09-19 19:11:32 +0100

    added db stuff and made timeline connect to db

--- src/app/components/timeline.tsx
+++ src/app/components/timeline.tsx
@@ -1,4 +1,11 @@
-export function Timeline(){
+import { prisma } from '@/db';
+
+export async function Timeline(){
+    const experience = await prisma.experience.findMany({
+        include:{
+            skills: true
+        }
+    });
     return (
         <main>
             <div className="absolute bottom-[10rem] left-0 right-0 flex justify-between items-center px-4">
@@ -8,10 +15,10 @@ export function Timeline(){
                 
                 {/* Timeline markers */}
                 <div className="absolute inset-0 flex items-center justify-between">
-                    <div className="w-4 h-4 bg-slate-900 rounded-full"></div>
-                    <div className="w-4 h-4 bg-slate-900 rounded-full"></div>
-                    <div className="w-4 h-4 bg-slate-900 rounded-full"></div>
-                    {/* Add more markers as needed */}
+                    {experience.map((a, index) =>(
+                        <a href='#' key={index} className='w-4 h-4 bg-slate-900 rounded-full'></a>
+                    ))}
+                    
                 </div>
                 </div>
             </div>
